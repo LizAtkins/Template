28b9eff833a63d707fa7ea66fa4f2200050e488a
Elizabeth Atkins <elizabeth.atkins@sinclair.edu>
2022-12-04 20:41:14 -0500

    Add project files.

---
+++ Template/Source.cpp
@@ -0,0 +1,20 @@
+// Elizabeth Atkins
+// CIS 1202 C++ Software Development
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main() {
+
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
+
