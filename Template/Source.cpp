e81242e7276418e0be4da637bb48e36061737ac5
Elizabeth Atkins <elizabeth.atkins@sinclair.edu>
2022-12-04 20:48:55 -0500

    Added sample data

--- Template/Source.cpp
+++ Template/Source.cpp
@@ -9,15 +9,15 @@ using namespace std;
 
 template <typename T>
 double half(T temp) {
-	return temp / 2.0
+	return temp / 2.0;
 }
 
 int main() {
-	double a;
-	float b;
-	int c;
+	double a = 5.7;
+	float b = 6.9f;
+	int c = 7;
 
-	cout << setprecision(5) << showpoint;
+	cout << setprecision(3) << showpoint;
 	cout << half(a) << endl << half(b) << endl << ceil(half(c)) << endl;
 
 
